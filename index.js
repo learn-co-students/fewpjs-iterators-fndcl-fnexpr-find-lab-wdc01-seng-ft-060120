3e9110306fd0eecac323a0779c61a48ea9140560
andy-aguilar <aaguil3@gmail.com>
2020-07-21 20:23:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let yearObject = array.find(function(e) {return e.result === "W"})
+  if (yearObject){
+  return yearObject.year}
+  else{return undefined}
+}
