b0278b3cde80f31397249ecf77a3e8fc0cbed077
Kosi Akporji <kosiazom@gmail.com>
2020-08-07 15:06:41 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+let result = array.find(result => result.result === 'W')
+if (result) {
+    return result.year
+}
+}
